27e5c5b824c1aa5ac41f12a59f8cdc2f3d857523
Danllpz <danillp@gmail.com>
2025-03-12 10:01:59 -0600

    Practica02 Replica el puerto A en el puerto B

---
+++ Practica02/PuertoA_sale_por_PuertoB.asm
@@ -0,0 +1,20 @@
+__CONFIG _CP_OFF & _WDT_OFF & _PWRTE_ON & _XT_OSC
+						;Lo que esta en el puerto A sale por el puerto B
+LIST P=16F84A
+INCLUDE <P16F84A.INC>
+
+
+ORG 0
+
+Inicio 					;Configurar los pines de entrada y salida
+	bsf 	STATUS,RP0	;entramos en modo config
+	clrf 	TRISB		;Config el puerto B como salida
+	movlw	b'00011111'
+	movwf	TRISA
+	bcf		STATUS,RP0	;sale del modo configuracion
+	
+Principal
+	movf 	PORTA,w
+	movwf	PORTB
+	goto Principal
+END	
